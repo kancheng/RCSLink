7d27c672c8c328699bd0b45729951ea81ad1ce2d
kancheng <zxdfgcv@gmail.com>
2017-11-12 16:30:48 +0800

    Upload Code

---
+++ RCalculator/RCalculator/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using RDotNet;
+
+namespace RCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string result;
+            string input;
+            REngine engine;
+
+            /* init the R engine */           
+            REngine.SetEnvironmentVariables();
+            engine = REngine.GetInstance();
+            engine.Initialize();
+
+            /* input */
+            Console.WriteLine("Please enter the calculation");
+            input = Console.ReadLine();
+
+            /* calculate */
+            CharacterVector vector = engine.Evaluate(input).AsCharacter();
+            result = vector[0];
+
+            /* clean up */
+            engine.Dispose();
+
+            /* output */
+            Console.WriteLine("");
+            Console.WriteLine("Result: '{0}'", result);
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
